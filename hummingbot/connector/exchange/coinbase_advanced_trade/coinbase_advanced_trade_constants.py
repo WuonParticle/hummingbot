4003d57b24728d1504ab66e4cb9497a945edb62b
Warren <warren.t.ferrell@gmail.com>
2025-04-14 12:42:41 -0600

    switch to fixing Coinbase connector by switching to the proper public products endpoint.

--- hummingbot/connector/exchange/coinbase_advanced_trade/coinbase_advanced_trade_constants.py
+++ hummingbot/connector/exchange/coinbase_advanced_trade/coinbase_advanced_trade_constants.py
@@ -45,13 +45,16 @@ SIGNIN_ENDPOINTS = {
     CRYPTO_CURRENCIES_EP,
 }
 
-# Private API endpoints
+# Public API endpoints
 SERVER_TIME_EP = "/brokerage/time"
-ALL_PAIRS_EP = "/brokerage/products"
-PAIR_TICKER_EP = "/brokerage/products/{product_id}"
-PAIR_TICKER_RATE_LIMIT_ID = "PairTicker"
-PAIR_TICKER_24HR_EP = "/brokerage/products/{product_id}/ticker"
-PAIR_TICKER_24HR_RATE_LIMIT_ID = "PairTicker24Hr"
+ALL_PAIRS_EP = "/brokerage/market/products"  # https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getpublicproducts
+PAIR_TICKER_24HR_EP = "/brokerage/market/products/{product_id}/ticker"
+PAIR_TICKER_24HR_RATE_LIMIT_ID = "ProductTicker24Hr"
+
+# Private API endpoints
+PRIVATE_PRODUCTS_EP = "/brokerage/products"  # https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getproducts
+PRIVATE_PAIR_TICKER_24HR_EP = "/brokerage/products/{product_id}/ticker"
+PRIVATE_PAIR_TICKER_24HR_RATE_LIMIT_ID = "PrivatePairTicker24Hr"
 ORDER_EP = "/brokerage/orders"
 BATCH_CANCEL_EP = "/brokerage/orders/batch_cancel"
 GET_ORDER_STATUS_EP = "/brokerage/orders/historical/{order_id}"
@@ -65,9 +68,8 @@ ACCOUNT_RATE_LIMIT_ID = "Account"
 SNAPSHOT_EP = "/brokerage/product_book"
 
 PRIVATE_REST_ENDPOINTS = {
-    ALL_PAIRS_EP,
-    PAIR_TICKER_RATE_LIMIT_ID,
-    PAIR_TICKER_24HR_RATE_LIMIT_ID,
+    PRIVATE_PRODUCTS_EP,
+    PRIVATE_PAIR_TICKER_24HR_RATE_LIMIT_ID,
     ORDER_EP,
     BATCH_CANCEL_EP,
     GET_ORDER_STATUS_RATE_LIMIT_ID,
@@ -81,6 +83,8 @@ PRIVATE_REST_ENDPOINTS = {
 
 PUBLIC_REST_ENDPOINTS = {
     SERVER_TIME_EP,
+    ALL_PAIRS_EP,
+    PAIR_TICKER_24HR_RATE_LIMIT_ID,
 }
 
 WS_HEARTBEAT_TIME_INTERVAL = 30
@@ -169,3 +173,17 @@ RATE_LIMITS = [
 RATE_LIMITS.extend(PRIVATE_REST_RATE_LIMITS)
 RATE_LIMITS.extend(PUBLIC_REST_RATE_LIMITS)
 RATE_LIMITS.extend(SIGNIN_RATE_LIMITS)
+
+
+def get_products_endpoint(use_auth_for_public_endpoints: bool) -> str:
+    if use_auth_for_public_endpoints:
+        return PRIVATE_PRODUCTS_EP
+    else:
+        return ALL_PAIRS_EP
+
+
+def get_ticker_endpoint(use_auth_for_public_endpoints: bool) -> Tuple[str, str]:
+    if use_auth_for_public_endpoints:
+        return (PRIVATE_PAIR_TICKER_24HR_EP, PRIVATE_PAIR_TICKER_24HR_RATE_LIMIT_ID)
+    else:
+        return (PAIR_TICKER_24HR_EP, PAIR_TICKER_24HR_RATE_LIMIT_ID)
